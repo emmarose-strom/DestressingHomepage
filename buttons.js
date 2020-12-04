d5411fcb6dad558afe9358656a1eb758f373db52
emmarose-strom <74164723+emmarose-strom@users.noreply.github.com>
2020-12-03 21:39:34 -0500

    Add files via upload

---
+++ buttons.js
@@ -0,0 +1,12 @@
+document.querySelector('#home').onclick = function(){
+    window.location.href = 'https://ad6324bc-2680-4c61-bea8-7f38c6bd9b0c-ide.cs50.xyz:8080/homepage.html';
+}
+document.querySelector('#me').onclick = function(){
+    window.location.href = 'https://ad6324bc-2680-4c61-bea8-7f38c6bd9b0c-ide.cs50.xyz:8080/self.html';
+}
+document.querySelector('#friend').onclick = function(){
+    window.location.href = 'https://ad6324bc-2680-4c61-bea8-7f38c6bd9b0c-ide.cs50.xyz:8080/friend.html';
+}
+document.querySelector('#ed').onclick = function(){
+    window.location.href = 'https://ad6324bc-2680-4c61-bea8-7f38c6bd9b0c-ide.cs50.xyz:8080/ed.html';
+}
